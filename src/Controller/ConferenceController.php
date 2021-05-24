60d166e741ac05cd39a141bf004321812b1c1f00
Zeggriim <51760726+zeggriim2@users.noreply.github.com>
2021-05-25 00:34:18 +0200

    6 Création du controller

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/hello/{name}", name="home")
+     */
+    public function index(string $name = ''): Response
+    {
+        $greet = '';
+
+        if ($name){
+            $greet = sprintf('<h1>Hello %s</h1>', htmlspecialchars($name));
+        }
+
+        return new Response(<<<EOF
+<html>
+    <body>
+    $greet
+        <img src="/images/under-construction.gif" />
+    </body>
+</html>
+EOF
+        );
+//        return $this->render('conference/index.html.twig', [
+//            'controller_name' => 'ConferenceController',
+//        ]);
+         
+    }
+}
