d20daf3250fd828d93a109cb4b2a5b026eae6c03
Zeggriim2 <51760726+zeggriim2@users.noreply.github.com>
2021-05-26 02:12:41 +0200

    Ajout du bundle EasyAdmin + Doctrine + Entity Conférence/Comment + Génération des migrations

--- dump.sql
+++ dump.sql
@@ -1,4 +1,4 @@
-    --
+--
 -- PostgreSQL database dump
 --
 
@@ -15,6 +15,47 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- Data for Name: conference; Type: TABLE DATA; Schema: public; Owner: main
+--
+
+COPY public.conference (id, city, year, is_international) FROM stdin;
+1	Lyon	2018	t
+2	Paris	2019	f
+\.
+
+
+--
+-- Data for Name: comment; Type: TABLE DATA; Schema: public; Owner: main
+--
+
+COPY public.comment (id, conference_id, author, text, email, created_at, photo_filename) FROM stdin;
+\.
+
+
+--
+-- Data for Name: doctrine_migration_versions; Type: TABLE DATA; Schema: public; Owner: main
+--
+
+COPY public.doctrine_migration_versions (version, executed_at, execution_time) FROM stdin;
+DoctrineMigrations\\Version20210525233911	2021-05-25 23:40:48	122
+\.
+
+
+--
+-- Name: comment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: main
+--
+
+SELECT pg_catalog.setval('public.comment_id_seq', 1, false);
+
+
+--
+-- Name: conference_id_seq; Type: SEQUENCE SET; Schema: public; Owner: main
+--
+
+SELECT pg_catalog.setval('public.conference_id_seq', 2, true);
+
 
 --
 -- PostgreSQL database dump complete
---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+//        return parent::index();
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+        $url = $routeBuilder->setController(ConferenceCrudController::class)->generateUrl();
+
+        return $this->redirect($url);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('GuestBook');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToRoute('Back to the website', 'fas fa-home', 'homepage');
+        yield MenuItem::linkToCrud("Conferences","fas fa-map-marker-alt",Conference::class);
+        yield MenuItem::linkToCrud("Commetns","fas fa-comments",Comment::class);
+        // yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
